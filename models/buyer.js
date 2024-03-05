efa262a2de00717d2d7c3c52c332bebe265a8cc0
hxnshaw <eghosasere07@gmail.com>
2024-03-05 09:49:37 +0100

    buyer password

--- models/buyer.js
+++ models/buyer.js
@@ -1,7 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+
+const bcrypt = require("bcrypt");
 module.exports = (sequelize, DataTypes) => {
   class Buyer extends Model {
     /**
@@ -13,32 +13,50 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Buyer.init({
-    email: {
-      type:DataTypes.STRING,
-      required:true,
-      unique:true,
-      allowNull:false,
-      validate:{
-        isEmail:{message:'Please Provide A Valid Email Address'}
-      }
+  Buyer.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        required: true,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: { message: "Please Provide A Valid Email Address" },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        required: true,
+        allowNull: false,
+        validate: {
+          min: 5,
+        },
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "buyer",
+      },
     },
-    password: {
-      type:DataTypes.STRING,
-      required:true,
-      allowNull:false,
-      validate:{
-        min:5
-      }
-    },
-    role: {
-      type:DataTypes.STRING,
-      allowNull:false,
-      defaultValue:"buyer"
+    {
+      sequelize,
+      modelName: "Buyer",
     }
-  }, {
-    sequelize,
-    modelName: 'Buyer',
+  );
+
+  //hash buyer password
+  Buyer.addHook("beforeSave", async function (buyer) {
+    if (!buyer.changed("password")) return;
+    const salt = await bcrypt.genSaltSync(10);
+    clerk.password = await bcrypt.hashSync(buyer.password, salt);
   });
+
+  //method to check user entered the correct password
+  Buyer.prototype.comparePassword = async function (buyerPassword) {
+    const buyer = this;
+    const isMatch = await bcrypt.compare(buyerPassword, buyer.password);
+    if (!isMatch) throw new Error("Invalid Credentials");
+    return isMatch;
+  };
   return Buyer;
 };
